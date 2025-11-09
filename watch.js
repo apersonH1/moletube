09a0ba1990deca37dfb9c3f0dabe2057c4be1e52
apersonH1 <iamjonhiuwu@gmail.com>
2025-11-09 15:32:08 -0500

    Delete watch.js

--- watch.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const url = params.get("url");
-const title = params.get("title");
-const channel = params.get("channel");
-
-document.getElementById("watchFrame").src = url;
-document.getElementById("videoTitle").textContent = title || "Untitled";
-document.getElementById("videoChannel").textContent = channel || "Unknown Channel";
